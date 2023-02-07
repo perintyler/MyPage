df5730dcc2d6d0aa27066a23009ad0b2ea3d36ee
perintyler <perintyler@gmail.com>
2023-02-07 13:58:22 -0500

    simple list of projects with hosted websites

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+          <li><a href="crypto-books.com">crypto-books.com</a></li>
+          <li><a href="poetry-parser.com"> poetry-parser.com</a></li>
+          <li><a href="arch-app.com"> arch-app.com </a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
