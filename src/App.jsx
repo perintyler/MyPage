886c42e050b229fa85b0734c533e0039bee2a18d
perintyler <perintyler@gmail.com>
2023-02-10 15:22:07 -0500

    main view

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,99 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import SvgIcon from '@mui/material/SvgIcon';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import { ReactComponent as WebsiteLogo } from './TP.svg';
+import { ReactComponent as GithubIcon } from './github.svg';
+import { CSSTransition } from 'react-transition-group';
+
+import ProjectGrid from './ProjectGrid'
+import { LogoAnimation, FadeInContainer } from './Animations';
+
+import PROJECTS from './Projects';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <ul>
-          <li><a href="crypto-books.com">crypto-books.com</a></li>
-          <li><a href="poetry-parser.com"> poetry-parser.com</a></li>
-          <li><a href="arch-app.com"> arch-app.com </a></li>
-      </ul>
-    </div>
-  );
+const RED = "#FF2467";
+
+function LogoBox()
+{
+    return (
+        <Box margin={2}>
+            <img src="TP.png" style={{width: "75px", height: "75px"}}/>
+        </Box>
+    );
 }
 
-export default App;
+function GithubSVG({ width, height, color })
+{
+    return (
+        <Box margin={0} width={width} height={height} display="flex" justifyContent="center">
+            <SvgIcon 
+              sx={{color: color, textAlign: "center", width: width, height: height}} 
+              component={GithubIcon} 
+            />
+        </Box>
+    );
+}
+
+function GreetingBox()
+{
+    return (
+        <Box margin={4} paddingBottom={2} sx={{textAlign: "center"}}>
+            <Typography variant="h2" paddingLeft={1}><b>Hi. </b>👋</Typography>
+            <Typography variant="h1"><b>I'm Tyler.</b></Typography>
+            <Typography variant="h4" paddingTop={4} paddingBottom={5}>
+                You can reach me at <Link>tyler@perin.email</Link>
+            </Typography>
+            <a href="https://github.com/perintyler" style={{textDecoration: "none"}}>
+                <Button variant="outlined" sx={{borderColor: "#551A8B", padding: "15px"}}>
+                    <GithubSVG width={36} height={36} color="#551A8B" /> 
+                    <Typography paddingLeft={1} color="#551A8B">github.com/perintyler</Typography>
+                </Button>
+            </a>
+        </Box>
+    )
+}
+
+export default class App extends React.Component {
+
+    state = { ready: false };
+
+    render()
+    {
+        const logoAnimationBox = (
+            <Box 
+              display="flex" 
+              justifyContent="center" 
+              alignItems="center"
+              height="60vw" 
+              width="100vw"
+              children={<LogoAnimation onDone={()=>this.setState({ready:true})} />}
+            />
+        );
+
+        const mainViewBox = (
+            <FadeInContainer hide={!this.state.ready}>
+                <Box 
+                  margin={2}
+                  children={<img src="TP.png" style={{width: "75px", height: "75px"}} />}
+                />
+                <Box paddingBottom="20px">
+                    <GreetingBox />
+                    <Box 
+                      paddingTop={1} 
+                      marginLeft={2} 
+                      marginRight={2}
+                      children={<ProjectGrid projects={PROJECTS} />}
+                    />
+                </Box>
+            </FadeInContainer>
+        );
+
+        return <div className="App">{ !this.state.ready ? logoAnimationBox : mainViewBox }</div>;
+    }
+}
