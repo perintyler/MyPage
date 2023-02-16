3661bc0e59405b1230a66ecc07b4213eeb6f59b5
perintyler <perintyler@gmail.com>
2023-02-16 18:57:56 -0500

    github button in top right corner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
@@ -7,18 +8,22 @@ import SvgIcon from '@mui/material/SvgIcon';
 import { ReactComponent as GithubIcon } from './github.svg';
 
 import ProjectGrid from './ProjectGrid'
-import PROJECTS from './Projects';
+import getProjects from './Projects';
+import COLORS from './Colors';
 import { LogoAnimation, FadeInContainer } from './Animations';
 import './App.css';
 
-function GithubSVG({ width, height, color })
+function GithubSVG({ width, height, color, hoverColor })
 {
     return (
-        <Box margin={0} width={width} height={height} display="flex" justifyContent="center">
-            <SvgIcon 
-              sx={{color: color, textAlign: "center", width: width, height: height}} 
-              component={GithubIcon} 
-            />
+        <Box width={width} height={height} display="flex" justifyContent="center">
+            <SvgIcon component={GithubIcon} sx={{
+                color: color, 
+                "&:hover": {color: hoverColor}, 
+                textAlign: "center", 
+                width: width, 
+                height: height
+            }} />
         </Box>
     );
 }
@@ -29,7 +34,7 @@ function GreetingBox()
         <Box margin={4} paddingBottom={2} sx={{textAlign: "center"}}>
             <Typography variant="h2" paddingLeft={1}><b>Hi 👋</b></Typography>
             <Typography variant="h4" paddingTop={4} paddingBottom={5}>
-                You can reach me at <Link>tyler@perin.email</Link>
+                You can reach me at <Link href="mailto: tyler@perin.email">tyler@perin.email</Link> 
             </Typography>
         </Box>
     )
@@ -53,21 +58,36 @@ function IntroAnimationView({ onDone })
     );
 }
 
+function HeaderBar()
+{
+    return (
+        <Grid container width="100%" direction="row" alignItems="flex-start" justifyContent="space-between" >
+            <Grid item justify="flex-start"> 
+                <Box
+                  margin={2}
+                  children={<img src="TP.png" alt="logo" width="120px" height="120px" />}
+                />
+            </Grid>
+            <Grid item justify="flex-end" margin={1} paddingTop={1}> 
+                <a href="https://github.com/perintyler">
+                    <GithubSVG width="50px" height="50px" color={COLORS.purple} hoverColor={COLORS.blue} />
+                </a>
+            </Grid>
+        </Grid>
+    );
+}
 function PortfolioView({ hide })
 {
     return (
         <FadeInContainer hide={hide}>
-            <Box 
-              margin={2}
-              children={<img src="TP.png" alt="logo" style={{width: "75px", height: "75px"}} />}
-            />
+            <HeaderBar />
             <Box paddingBottom="50px">
                 <GreetingBox />
                 <Box 
                   paddingTop={1} 
                   marginLeft={2} 
                   marginRight={2}
-                  children={<ProjectGrid projects={PROJECTS} />}
+                  children={<ProjectGrid projects={getProjects()} />}
                 />
             </Box>
             <Box 
