fc9717f746c12d463853d5199699d0039c79fee4
perintyler <perintyler@gmail.com>
2023-02-16 18:57:24 -0500

    color scheme object

---
+++ src/Colors.js
@@ -0,0 +1,11 @@
+/* Colors.js */
+
+const COLORS = {
+    red    : "#ff2467",
+    black  : "#151515",
+    purple : "#531cb3",
+    gray   : "#9a98b5",
+    blue   : "#6494aa"
+};
+
+export default COLORS;
