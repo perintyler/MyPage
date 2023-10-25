69df9c1c410ac9cf6f170096a1fca84592b5a981
perintyler <perintyler@gmail.com>
2023-10-25 19:46:13 -0400

    add more colors

--- src/Colors.js
+++ src/Colors.js
@@ -1,11 +1,15 @@
 /* Colors.js */
 
 const COLORS = {
-    red    : "#ff2467",
-    black  : "#151515",
-    purple : "#531cb3",
-    gray   : "#9a98b5",
-    blue   : "#6494aa"
+    red      : "#ff2467",
+    black    : "#151515",
+    purple   : "#531cb3",
+    purple2  : "#9E3FFD",
+    gray     : "#9a98b5",
+    blue     : "#6494aa",
+    blue2    : "#136DFF",
+    white    : "#FFFFFF",
+    offwhite : "#F9FAFA"
 };
 
 export default COLORS;
