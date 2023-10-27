9115fb6d2860af0031f6f15ff72626648cd9e00e
perintyler <perintyler@gmail.com>
2023-10-27 18:23:00 -0400

    success or error dialog for emailjs results

--- src/ContactView.jsx
+++ src/ContactView.jsx
@@ -1,16 +1,39 @@
 
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { SimpleContactForm } from './ContactForms';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Dialog from '@mui/material/Dialog';
+import Button from '@mui/material/Button';
+import { ContactForm } from './ContactForm';
 import { FadeInContainer } from './Animations';
-import { newMessage } from './EmailApi';
+import EmailApi from './EmailApi';
 
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
 });
+
+function EmailSentDialog({ show, success, onClose })
+{
+  const successMessage = "Your message was successfully sent";
+  const failureMessage = "Something went wrong... Your message failed to send.";
+
+  let severity = success === true ? "success" : "error";
+
+  return (
+    <Dialog open={show} transitionDuration={0}>
+      <Alert severity={severity} variant="filled">
+        <AlertTitle style={{textTransform:'capitalize'}}>{severity}</AlertTitle>
+        <div>{success === true ? successMessage : failureMessage}</div>
+      </Alert>
+      <Button color={severity} width="100%" onClick={()=>onClose()}>OK</Button>
+    </Dialog>
+  );
+}
 
 function ContactViewContainer({ children })
 {
@@ -51,14 +74,34 @@ function ContactViewContainer({ children })
 
 export default function ContactView()
 {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  function resetState() {
+    setIsDialogOpen(false);
+    setSuccess(false);
+  }
+
+  function handleSubmission(contactInfo) {
+    EmailApi.new_message(contactInfo, function onSuccess() {
+      setSuccess(true);
+      setIsDialogOpen(true);
+    }, function onFailure() {
+      setSuccess(false);
+      setIsDialogOpen(true);
+    });
+  }
+
   return (
     <ContactViewContainer>
       <h1 style={{marginTop: 0, textAlign: "center"}}>Contact</h1>
-      <SimpleContactForm
+      <ContactForm
         buttonTitle="send"
-        onComplete={(contactInfo) => newMessage(contactInfo)}
+        onComplete={(contactInfo) => handleSubmission(contactInfo)}
         msgIsRequired={true}
+        phoneIsRequired={false}
       />
+      <EmailSentDialog show={isDialogOpen} success={success} onClose={()=>resetState()} />
     </ContactViewContainer>
   );
 }
