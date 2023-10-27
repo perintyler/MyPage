9b13b2b1857b4c87a3f07453a73f9afbcae496a1
perintyler <perintyler@gmail.com>
2023-10-27 10:09:23 -0400

    tutoring sign up form

--- src/tutoring/SignupForm.jsx
+++ src/tutoring/SignupForm.jsx
@@ -1,118 +1,186 @@
 
 import { useState, useRef } from "react";
 import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
-import MultiStep from 'react-multistep';
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Slide from '@mui/material/Slide';
+import { Stepper } from 'react-form-stepper';
 import ContactForm from '../ContactForm';
 import './SignupForm.css';
+
+const MINIMUM_GRADE = 6;
+const MAXIMUM_GRADE = 12; // inclusive
+const SIGNUP_STEPS = ["Select Grade", "Select Course", "Contact Info", "Success"];
 
 class SignupInfo 
 {
     constructor() {
-        this.isParent = null;
         this.service = null;
-        this.userEmail = null;
-        this.userPhone = null; // optional
-        this.userMessage = null;
+        this.grade = null;
+        this.contactInfo = null;
     }
-    
-    
+
+    isValid() {
+        return this.service != null && this.grade != null && this.contactInfo != null;
+    }
+
+    pretty() {
+        return `<SignupInfo service='${this.service}' grade='${this.grade}' contactInfo=${this.contactInfo.pretty()}>`
+    }
 }
 
-function SignupInputBox({ title, subtitle, children })
+function SignupStepContainer({ title, subtitle, children, stepBack, margin })
 {
-  return (
-      <Box className="signup-input-box">
-          <h1>{title}</h1>
-          <p>{subtitle}</p>
-          <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">{ children }</Box>
-      </Box>
-  );
-}
+    var backArrow = stepBack === undefined ? <Box className="back-arrow" /> : (
+        <IconButton className="back-arrow" onClick={()=>stepBack()} >
+            <ArrowBackIcon/>
+        </IconButton>
+    );
 
-function SignupInfoButton({ onClick, title })
-{
+    var header = (
+        <div className="signup-step-header">
+            {title    === undefined ? <div /> : <h2>{ title }</h2>}
+            {subtitle === undefined ? <div /> : <h4>{ subtitle }</h4>}
+        </div>
+    );
+
+    var inputs = <Box mt={margin || "24px"} className="signup-input-column">{children}</Box>;
+
     return (
-        <Box m={1}>
-            <Button className="signup-info-button" variant="contained" onClick={onClick}>{ title }</Button>
+        <Box className="signup-step">
+            <Slide direction="right" in={true} timeout={600} mountOnEnter unmountOnExit>
+                <Card sx={{boxShadow: 5}} className="signup-input-card">{backArrow}{header}{inputs}</Card>
+            </Slide>
         </Box>
     );
 }
 
-// ------------------------------------------------------------------------------------------
-
-/* Signup Step #1 */
-function ParentOrChildInputBox({ signupInfo, goToNextStep })
+function SignupFormButton({ onClick, title })
 {
-  function completeForm1(isParent) {
-      signupInfo.isParent=isParent;
-      goToNextStep();
-  }
-  
-  return (
-      <SignupInputBox title="Signup" subtitle="Who needs Tutoring?">
-          <SignupInfoButton onClick={()=>completeForm1(false)} title="I do" />
-          <SignupInfoButton onClick={()=>completeForm1(true)} title="My child" />
-      </SignupInputBox>
-  );
+    return (
+        <Box m={1} width="100%">
+            <Button fullWidth={true} className="signup-info-button" variant="contained" onClick={onClick}>{ title }</Button>
+        </Box>
+    );
 }
 
-/* Signup Step #2 */
-function CourseInputBox({ signupInfo, goToNextStep })
-{
-    function completeForm2(service) {
-        signupInfo.service = service; 
-        goToNextStep();
-    }
+// ==============================================================================================================
 
+/**
+ ** Signup Step #1 
+ **/
+function GradeInputBox({ signupInfo, stepBack, stepForward })
+{
+    var allowedGrades = Array.from({length: MAXIMUM_GRADE-MINIMUM_GRADE+1}, (_, index) => index+MINIMUM_GRADE);
+
+    var gradeButtons = allowedGrades.map((grade) => (
+        <SignupFormButton key={grade} title={`${grade}th Grade`} onClick={() => {
+            signupInfo.current.grade = grade;
+            stepForward();
+        }} />
+    ));
+    
+    return <SignupStepContainer title="What grade are you in?" stepBack={stepBack} children={gradeButtons} />;
+}
+
+/**
+ ** Signup Step #2 
+ **/
+function CourseInputBox({ signupInfo, stepBack, stepForward })
+{
     function SelectTutoringServiceButton({ service }) {
-        return <SignupInfoButton onClick={()=>completeForm2(service)} title={service} />
+        return <SignupFormButton title={service} onClick={()=>{
+            signupInfo.current.service = service; 
+            stepForward();
+        }} />;
     }
 
     return (
-        <SignupInputBox title="Course" subtitle="Which course do you need tutoring for?">
+        <SignupStepContainer title="What course do you need tutoring for?" stepBack={stepBack}>
             <SelectTutoringServiceButton service="AP Computer Science" />
-            <SelectTutoringServiceButton service="Beginner Python Course" />
-            <SelectTutoringServiceButton service="Intermediate Python Course" />
-            <SelectTutoringServiceButton service="Web Design Course" />
+            <SelectTutoringServiceButton service="Beginner Python" />
+            <SelectTutoringServiceButton service="Intermediate Python" />
+            <SelectTutoringServiceButton service="Data Structures & Algorithms" />
+            <SelectTutoringServiceButton service="Web Design" />
             <SelectTutoringServiceButton service="Personalized Curiculum" />
             <SelectTutoringServiceButton service="Other" />
-        </SignupInputBox>
+        </SignupStepContainer>
     );
 }
 
-/* Signup Step #3 */
-function ContactInputBox({ signupInfo, goToNextStep })
+/**
+ ** Signup Step #3
+ **/
+function ContactInputBox({ signupInfo, stepBack, stepForward })
 {
-  return (
-    <SignupInputBox title="Reach out" subtitle="Enter your contact info and send me a message.">
-      <ContactForm />
-    </SignupInputBox>
-  );
-}
-
-export default function SignupForm({ onComplete }) 
-{
-  const [signupStep, setSignupStep] = useState(1);
-  const signupInfo = useRef(new SignupInfo());
-
-  var SignupForm;
-
-  if (signupStep === 1) {
-    SignupForm = ParentOrChildInputBox;
-  } else if (signupStep === 2) {
-    SignupForm = CourseInputBox;
-  } else if (signupStep === 3) {
-    SignupForm = ContactInputBox;
-  } else {
     return (
-        <Box>
-            <h1>Signup Complete</h1>
-            <p>I will get back to you within the next 2-3 business days</p>
-            <Button onClick={()=>{}}>OK</Button>
-        </Box>
+        <SignupStepContainer 
+          stepBack={stepBack} 
+          title="How can I reach you?" 
+          subtitle="Add a message if you there's anything else you'd like to tell me"
+          margin="0px"
+        >
+            <ContactForm buttonTitle="Complete Signup" onComplete={(contactInfo) => {
+                signupInfo.current.contactInfo = contactInfo;
+                stepForward();
+            }} />
+        </SignupStepContainer>
     );
-  }
+}
 
-  return <SignupForm signupInfo={signupInfo} goToNextStep={()=>setSignupStep(signupStep+1)} />
+/**
+ ** Signup Step #4
+ **/
+function SignupCompleteBox({ signupInfo, stepBack, stepForward })
+{
+    const title = "Signup Complete";
+    const subtitle = "I will get back to you within 1-2 business days";
+
+    console.log(`New SignupForm: ${signupInfo.current.pretty()}`);
+
+    return (
+        <SignupStepContainer className="signup-complete-box" title={title} subtitle={subtitle}>
+            <Box mb="20px">
+                <Button variant="outlined" onClick={()=>stepForward(signupInfo)}>OK</Button>
+            </Box>
+        </SignupStepContainer>
+    );
+}
+
+// ==============================================================================================================
+
+export default function SignupForm({ onComplete, onCancel }) 
+{
+    const [stepNumber, setStepNumber] = useState(0);
+    const signupInfo = useRef(new SignupInfo());
+    const allSignupSteps = [GradeInputBox, CourseInputBox, ContactInputBox, SignupCompleteBox]
+
+    function stepForward() 
+    {
+        if (stepNumber === allSignupSteps.length-1) {
+            onComplete(signupInfo);
+        } else {
+            setStepNumber(stepNumber+1);
+        }
+    }
+
+    function stepBack() 
+    {
+        if (stepNumber === 0) {
+            onCancel();
+        } else {
+            setStepNumber(stepNumber-1)
+        }
+    }
+
+    var SignupStep = allSignupSteps[stepNumber];
+
+    return (
+      <Box width="100%" display="flex" flexDirection="column" className="signup-form" pt="100px">
+        <Stepper steps={SIGNUP_STEPS.map(step => ({label: step}))} activeStep={stepNumber} />
+        <SignupStep signupInfo={signupInfo} stepForward={stepForward} stepBack={stepBack} />
+      </Box>
+    );
 }
