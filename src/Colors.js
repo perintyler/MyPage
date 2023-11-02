93135cdb72d4e4c3aad16507cc790f5935ca9cd0
perintyler <perintyler@gmail.com>
2023-11-01 20:49:39 -0400

    add more colors

--- src/Colors.js
+++ src/Colors.js
@@ -1,23 +1,31 @@
 /* Colors.js */
 
 const COLORS = {
-    red           : "#ff2467",
-    black         : "#151515",
-    purple        : "#531cb3",
-    purple2       : "#9E3FFD",
-    gray          : "#9a98b5",
-    blue          : "#6494aa",
-    blue2         : "#136DFF",
-    white         : "#FFFFFF",
-    offwhite      : "#F9FAFA",
-    maize         : "#FFF07C",
-    cyan          : "#00FFFF",
-    lightcoral    : "#E58C8A",
-    tearose       : "#EEC0C6",
-    timberwolf    : "#EADEDA",
-    burntumber    : "#823329",
-    cordovan      : "#8A3033",
-    goldengate    : "#EB4511"
+    red                 : "#ff2467",
+    black               : "#151515",
+    purple              : "#531cb3",
+    purple2             : "#9E3FFD",
+    gray                : "#9a98b5",
+    blue                : "#6494aa",
+    blue2               : "#136DFF",
+    white               : "#FFFFFF",
+    off_white           : "#F9FAFA",
+    maize               : "#FFF07C",
+    cyan                : "#00FFFF",
+    light_coral         : "#E58C8A",
+    tea_rose            : "#EEC0C6",
+    timber_wolf         : "#EADEDA",
+    burnt_umber         : "#823329",
+    cordovan            : "#8A3033",
+    golden_gate         : "#EB4511",
+    amethyst            : "#8661C1",
+    crimson             : "#D7263D",
+    burnt_sienna        : "#F96E46",
+    state_blue          : "#6665DD",
+    sping_green         : "#0CF574",
+    aureolin            : "#F9E900",
+    sun_glow            : "#FFD166",
+    lavender_blush      : "#F3E8EE"
 };
 
 export default COLORS;
